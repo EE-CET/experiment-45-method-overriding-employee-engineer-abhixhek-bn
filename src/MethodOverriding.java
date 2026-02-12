6164c39a525868452f74fd5029a791cd529577d6
Abhishek B <abhishekbn10991@gmail.com>
2026-02-12 09:25:21 +0530

    first commit

--- src/MethodOverriding.java
+++ src/MethodOverriding.java
@@ -1,16 +1,23 @@
 class Employee {
-   
+   void display(){
+        System.out.println("Name of class is Employee");
+   }
 }
 
 class Engineer extends Employee {
     // TODO: Override display() method
-    // 1. Print "Name of class is Engineer"
-    // 2. Call super.display() to print the parent's message
+    void display(){
+        System.out.println("Name of class is Engineer");
+        super.display();
+    }
+    
 }
 
 public class MethodOverriding {
-   
+    public static void main(String[] args){
+        Engineer C=new Engineer();
+        C.display();
         // TODO: Create an Engineer object
-        // TODO: Call the display() method
+    }   // TODO: Call the display() method
     
 }
